a90345f037a11b8d6eb3adc168f6dbe4d8867a41
Orestes Sanchez <orestes.sanchezbenavente@telefonica.com>
2024-07-18 17:42:11 +0200

    Refactor code.

--- esp-clouddetector.ino
+++ esp-clouddetector.ino
@@ -3,11 +3,7 @@
 #include "DHT.h"
 
 /*
-configuracion mlx cloud-detector
-
-#define hzMode 7 //0=0.5hz,1=1hz,2=2hz,3=4hz,4=8hz,5=16hz,6=32hz,7=64hz 
-#define adSensorResolution 1 //0=16bit,it 1=17bit, 2=18bit, 3=19b
-#define MLX90640_mirror false //this flips direction of sensor in case used in camera mode
+Author: Orestes Sanchez <miceno.atreides@gmail.com>
 */
 
 #define DHTPIN D6      // Digital pin connected to the DHT sensor
@@ -22,16 +18,21 @@ SerialCommand sCmd;  // The demo SerialCommand object
 // as the current DHT reading algorithm adjusts itself to work on faster procs.
 DHT dht(DHTPIN, DHTTYPE);
 
-const int IR_IMAGE_ROWS = 24;
+// MLX90640 camera is 32 x 24
 const int IR_IMAGE_COLS = 32;
+const int IR_IMAGE_ROWS = 24;
 const int frame_size = IR_IMAGE_COLS * IR_IMAGE_ROWS;
-const char *VERSION = "cloud-0.1.0";
 
+const char *VERSION = "cloud-0.2.0";
+
+// Internal variables for the status of the detector.
 float frame[frame_size];  // buffer for full frame of temperatures
 float temp = 0.0;         // current temperature from DHT22 sensor
 float humidity = 0.0;     // current humidity from DHT22 sensor
 boolean is_day = false;   // represents if it is day or night according to the sensor
 
+// start_SENSOR variables control if the controller is updating its internal
+// state using data from the sensor.
 boolean start_dht = true;
 boolean start_mlx = true;
 boolean start_day = true;
@@ -40,8 +41,10 @@ boolean start_day = true;
 //#define PRINT_TEMPERATURES
 #define PRINT_ASCIIART
 
-Adafruit_MLX90640 *setupMlx(Adafruit_MLX90640 *mlx,
-                            mlx90640_mode_t p_mode = MLX90640_CHESS,
+/*
+  MLX 90640 code
+*/
+Adafruit_MLX90640 *setupMlx(mlx90640_mode_t p_mode = MLX90640_CHESS,
                             mlx90640_resolution_t p_resolution = MLX90640_ADC_18BIT,
                             mlx90640_refreshrate_t p_refresh_rate = MLX90640_2_HZ) {
   if (!mlx->begin(MLX90640_I2CADDR_DEFAULT, &Wire)) {
@@ -57,7 +60,7 @@ Adafruit_MLX90640 *setupMlx(Adafruit_MLX90640 *mlx,
   return mlx;
 }
 
-void show_mlx_status(Adafruit_MLX90640 *mlx) {
+void show_mlx_status() {
   Serial.print(",mlx_serial=");
   Serial.print(mlx->serialNumber[0], HEX);
   Serial.print(mlx->serialNumber[1], HEX);
@@ -93,18 +96,7 @@ void show_mlx_status(Adafruit_MLX90640 *mlx) {
   Serial.println();
 }
 
-void setupSerial() {
-  Serial.begin(115200);
-  Serial.println();
-  Serial.print("Waiting for Serial line");
-  while (!Serial) {
-    Serial.print(".");
-    delay(10);
-  }
-  Serial.println();
-}
-
-void show_mlx_data(Adafruit_MLX90640 *mlx, float *frame) {
+void show_mlx_data() {
   Serial.println("===================================");
   Serial.print("MLX Ambient temperature = ");
   Serial.print(mlx->getTa(false));  // false = no new frame capture
@@ -113,7 +105,6 @@ void show_mlx_data(Adafruit_MLX90640 *mlx, float *frame) {
   Serial.println();
   show_frame(frame);
 }
-
 
 // Function for calculating median
 double Median(float *v, int n) {
@@ -172,10 +163,9 @@ void show_median(float *frame) {
   Serial.println();
 }
 
-void loopMlx(Adafruit_MLX90640 *mlx, float *frame) {
+void loopMlx() {
   if (start_mlx) {
     if (mlx && mlx->getFrame(frame) == 0) {
-      // show_mlx_data(mlx, (float *)&frame);
       // show_median(frame);
     } else {
       Serial.println("Failed");
@@ -185,79 +175,18 @@ void loopMlx(Adafruit_MLX90640 *mlx, float *frame) {
 }
 
 
+/* 
+  DHT code
+*/
 
-void setupSerialCommands() {
-  // Setup callbacks for SerialCommand commands
-  sCmd.addCommand("READ", send_data);    // Read summarized sensor data
-  sCmd.addCommand("IR", send_ir_image);  // Return IR data as a stream of float values
-  sCmd.addCommand("PING", show_ping);      // Echo current version
-  sCmd.addCommand("START", start_data_collection);  // Start data collection
-  sCmd.addCommand("STOP", stop_data_collection);  // Stop data collection
-  sCmd.setDefaultHandler(unrecognized);  // Handler for command that isn't matched  (says "NACK")
-  show_ping();
+void setupDHT() {
+  dht.begin();
 }
 
-void start_data_collection() {
-  char *arg;
-
-  arg = sCmd.next();
-  if (arg != NULL) {
-    char *sensor_to_start = arg;
-    if (strcmp(sensor_to_start, "DHT") == 0){
-      start_dht = true;
-    }
-    else if (strcmp(sensor_to_start, "MLX") == 0){
-      start_mlx = true;
-    }
-    else if (strcmp(sensor_to_start, "DAY") == 0){
-      start_day = true;
-    }
-    else{
-      return;
-    }
-    Serial.printf("STARTING %s SENSOR\n", arg);
+void loopDHT() {
+  if (start_dht) {
+    read_data();
   }
-  else {
-    Serial.println("STARTING ALL SENSORS");
-    start_dht = start_mlx = start_day = true;
-  }
-}
-
-void stop_data_collection() {
-  char *arg;
-
-  arg = sCmd.next();
-  if (arg != NULL) {
-    char *sensor_to_stop = arg;
-    if (strcmp(sensor_to_stop, "DHT") == 0){
-      start_dht = false;
-    }
-    else if (strcmp(sensor_to_stop, "MLX") == 0){
-      start_mlx = false;
-    }
-    else if (strcmp(sensor_to_stop, "DAY") == 0){
-      start_day = false;
-    }
-    else{
-      return;
-    }
-    Serial.printf("STOPING %s SENSOR\n", arg);
-  }
-  else {
-    Serial.println("STOPING ALL SENSORS");
-    start_dht = start_mlx = start_day = false;
-  }
-}
-
-void loopSerialCommands() {
-  sCmd.readSerial();  // We don't do much, just process serial commands
-}
-
-// This gets set as the default handler, and gets called when no other command matches.
-void unrecognized(const char *command) {
-  Serial.print("NACK[");
-  Serial.print(command);
-  Serial.println("]");
 }
 
 void read_data() {
@@ -273,6 +202,87 @@ void read_data() {
   temp = t;
 }
 
+/*
+  Serial commands
+*/
+void setupSerialCommands() {
+  // Setup callbacks for SerialCommand commands
+  sCmd.addCommand("READ", send_data);               // Read summarized sensor data
+  sCmd.addCommand("IR", send_ir_image);             // Return IR data as a stream of float values
+  sCmd.addCommand("PING", show_ping);               // Echo current version
+  sCmd.addCommand("START", start_data_collection);  // Start data collection
+  sCmd.addCommand("STOP", stop_data_collection);    // Stop data collection
+  sCmd.setDefaultHandler(unrecognized);             // Handler for command that isn't matched  (says "NACK")
+  show_ping();
+}
+
+/*
+  Start data collection command
+*/
+void start_data_collection() {
+  char *arg;
+
+  arg = sCmd.next();
+  if (arg != NULL) {
+    char *sensor_to_start = arg;
+    if (strcmp(sensor_to_start, "DHT") == 0) {
+      start_dht = true;
+    } else if (strcmp(sensor_to_start, "MLX") == 0) {
+      start_mlx = true;
+    } else if (strcmp(sensor_to_start, "DAY") == 0) {
+      start_day = true;
+    } else {
+      return;
+    }
+    Serial.printf("STARTING %s SENSOR\n", arg);
+  } else {
+    Serial.println("STARTING ALL SENSORS");
+    start_dht = start_mlx = start_day = true;
+  }
+}
+
+/*
+  Stop data collection command
+*/
+void stop_data_collection() {
+  char *arg;
+
+  arg = sCmd.next();
+  if (arg != NULL) {
+    char *sensor_to_stop = arg;
+    if (strcmp(sensor_to_stop, "DHT") == 0) {
+      start_dht = false;
+    } else if (strcmp(sensor_to_stop, "MLX") == 0) {
+      start_mlx = false;
+    } else if (strcmp(sensor_to_stop, "DAY") == 0) {
+      start_day = false;
+    } else {
+      return;
+    }
+    Serial.printf("STOPING %s SENSOR\n", arg);
+  } else {
+    Serial.println("STOPING ALL SENSORS");
+    start_dht = start_mlx = start_day = false;
+  }
+}
+
+void loopSerialCommands() {
+  sCmd.readSerial();  // We don't do much, just process serial commands
+}
+
+/*
+  Default handler for commands. This gets set as the default handler, and gets called when no other command matches.
+*/
+void unrecognized(const char *command) {
+  Serial.print("NACK[");
+  Serial.print(command);
+  Serial.println("]");
+}
+
+
+/*
+  Send sensor data over the serial line.
+*/
 void send_data() {
   Serial.print("cloud:");
   float median = Median(frame, frame_size);
@@ -289,11 +299,17 @@ void send_data() {
   Serial.println();
 }
 
+/*
+  Send raw IR data over the serial line.
+*/
 void send_ir_image() {
   Serial.print("ir:");
   show_frame(frame);
 }
 
+/*
+  Show status data over the serial line.
+*/
 void show_ping() {
   Serial.print(VERSION);
   Serial.printf(",dht=%d,mlx=%d,day=%d", start_dht, start_mlx, start_day);
@@ -303,34 +319,36 @@ void show_ping() {
   Serial.print(temp);
   Serial.print(",hum=");
   Serial.print(humidity);
-  show_mlx_status(mlx);
+  show_mlx_status();
   Serial.println();
 }
 
-void setupDHT() {
-  dht.begin();
-}
-
-void loopDHT() {
-  if (start_dht) {
-    read_data();
+/*
+  Serial setup
+*/
+void setupSerial() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Waiting for Serial line");
+  while (!Serial) {
+    Serial.print(".");
+    delay(10);
   }
+  Serial.println();
 }
 
 void setup() {
   setupSerial();
   delay(50);
   setupDHT();
-
-  Serial.println("Adafruit MLX90640 Simple Test");
   delay(50);
-  setupMlx(mlx);
+  setupMlx();
   setupSerialCommands();
 }
 
 void loop() {
   delay(50);
-  loopMlx(mlx, frame);
   loopDHT();
+  loopMlx();
   loopSerialCommands();
 }
